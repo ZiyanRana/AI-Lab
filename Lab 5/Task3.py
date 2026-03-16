8f35e5d3c956deddf0cdad6b5c7e9cc9ab54f3df
ZiyanRana <mziyanrana@gmail.com>
2026-03-16 14:45:43 +0500

    Completed Lab 5

--- Lab 5/Task3.py
+++ Lab 5/Task3.py
@@ -5,4 +5,26 @@ class PriorityQueue:
     def push(self, data, priority):
         self.queue.append([data, priority])
 
-    
+    def pop(self):
+        if len(self.queue) == 0:
+            return None
+
+        min_index = 0
+
+        for i in range(len(self.queue)):
+            if self.queue[i][1] < self.queue[min_index][1]:
+                min_index = i
+
+        return self.queue.pop(min_index)
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+pq = PriorityQueue()
+
+pq.push("Low Priority Task", 3)
+pq.push("High Priority Task", 1)
+pq.push("Medium Priority Task", 2)
+
+while not pq.is_empty():
+    print(pq.pop())
